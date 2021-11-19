a2814a7530e6920133e11c5c4b92f5a9d6a622a0
Jon Tokugawa <79482239+Joldon@users.noreply.github.com>
2021-11-19 14:46:27 +0100

    failed cors

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
 import './App.css';
+// import local json file
+// import jsondata from './assets/data.json';
+import data from'./assets/data.json';
+
+// console.log(data)
+// console.log(jsondata.fields)
+
 
 function App() {
+  const [articles, setArticles] = useState(data);
+
+  useEffect(() => {
+    const fetchArticles = async () => {
+      const res = await Axios.get(
+        "https://www.sicher-im-netz.de/siba-app/siba/list/0"
+      , {
+        method: 'GET',
+        // mode: 'no-cors',
+        headers: {
+          //  'Content-Type': 'application/x-www-form-urlencoded' 
+          // 'Access-Control-Allow-Origin': '*',
+          // "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
+          // "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token"
+          // 'Content-Type': 'application/json',
+        },
+        // withCredentials: true,
+        // credentials: 'same-origin',
+      })
+      setArticles(res.data.articles);
+      console.log(res);
+    };
+    fetchArticles();
+  }, []);
+  
+  // useEffect(() => {
+  //   fetch("https://www.sicher-im-netz.de/siba-app/siba/list/0")
+  //       .then(response => response.json())
+  //       .then(data => console.log(data)) 
+  //       }, [])
+  // console.log(articles)
+        
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Hello</h1>
     </div>
   );
 }
