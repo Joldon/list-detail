61a6727bfad7020ef7a7cf1c8aaaf5e3441efac2
Jon Tokugawa <79482239+Joldon@users.noreply.github.com>
2021-11-19 21:26:10 +0100

    detailed view correctly placed

--- src/components/DetailView.js
+++ src/components/DetailView.js
@@ -1,24 +1,24 @@
-import React from 'react'
-import {useParams} from 'react-router-dom'
+import React from "react";
+import { useParams } from "react-router-dom";
 
-const DetailView = ({articles}) => {
-    const {id} = useParams();
+const DetailView = ({ articles }) => {
+  const { id } = useParams();
 
-    const article = articles.filter(
-        (article) => id === article.id
-      );
-      console.log(articles)
-    
-      return article.length === 1 && (
-        <div className="detailed-view-container">
-          title={article[0].title}
-          level={article[0].level}
-          body={article[0].body}
-          {/* url={article[0].url} */}
-          publishedDate={article[0].published_date}
-        </div>
-      );
-    };
-    
-    export default DetailView;
-    
+  const article = articles.filter((article) => id === article.id);
+  console.log(articles);
+
+  return (
+    article.length === 1 && (
+      <div className="detailed-view">
+        <ul>
+          <li>{article[0].title}</li>
+          published date: {article[0].published_date}
+          <li>Level: {article[0].level}</li>
+        </ul>
+        <p>{article[0].body}</p>
+      </div>
+    )
+  );
+};
+
+export default DetailView;
